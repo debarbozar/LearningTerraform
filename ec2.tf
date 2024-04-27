35fe4c19ee0c8647ed21c042f53579cdc7493405
Debora <debora.barboza15@hotmail.com>
2024-04-26 22:54:21 -0300

    first lanch ec2

---
+++ ec2.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+#who is the provider
+provider "aws" {
+  region  = "us-east-1"
+}
+
+#resource of your ec2
+resource "aws_instance" "challenge_2" {
+  ami           = "ami-04e5276ebb8451442"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "LaunchEC2"
+  }
+}
+
+## Terraform init
+## Terraform plan
+## Terraform destroy
+## Terraform aplly
