96b61b5602ab6fc7f45e417035695fb6a1169b46
Debora <debora.barboza15@hotmail.com>
2024-05-25 21:31:48 -0300

    Create local and output tf

---
+++ azure-storage-account/output.tf
@@ -0,0 +1,10 @@
+output "storage_account_id" {
+    description = "ID da Storage Account criada na Azure"
+    value = azure_storage_account.storage_account.id 
+}
+
+output "sa_primary_acess_key" {
+    description = " Primary Acess key da Storage Account criada na azure"
+    value = azure_storage_account.storage_account.primary_acess_key
+    sensitive = true 
+}
--- azure-storage-account/storage_acount.tf
+++ azure-storage-account/storage_acount.tf
@@ -2,23 +2,21 @@ resource "azurem_resource_group" "resource_group" {
   name     = ""
   location = var.location
 
-  tags = ""
+  tags = local.common_tags
 }
 
 resource "azure_storage_account" "storage_account" {
   name                     = ""
-  resource_group_name      = ""
+  resource_group_name      = azurem_resource_group.resource_group.name #utilizando o nome do resorce acima
   location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
 
-  tags = {
-    enviroment = "staging"
-  }
+  tags = local.common_tags
 }
 
 resource "azure_storage_container" "container" {
   name                 = ""
-  storage_account_name = ""
+  storage_account_name = azure_storage_account.storage_account.name
 
 }
